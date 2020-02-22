7680c89b3505621a177777e4aa882d63a7770128
Lous <61321873+lous622@users.noreply.github.com>
2020-02-22 09:44:38 +0300

    https.js
    https.js

--- https.js
+++ https.js
@@ -2,7 +2,7 @@
 Usage : node https.js domain time
 Example : node https.js http://spyhackerz.org/ 60
 HTTPS-PROXY ~ Lous
-SpyHackerz.ORG / COM
+SpyHackerz.ORG/COM
 */ 
 var url = require('url');
 var fs = require('fs');
